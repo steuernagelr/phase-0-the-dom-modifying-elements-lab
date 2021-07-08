615de193b56c6539f4d6d8a4821adf6ad4b312b8
Rielly Steuernagel <reiguy@comcast.net>
2021-07-08 17:36:14 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.querySelector('main')
+main.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+document.body.appendChild(newHeader)
+
+newHeader.innerHTML = "Rielly is the champion"
+
